7fa71e6a106fc958a30332ee820ced8847345993
Nitin Prabhu <nitinprabhu@users.noreply.github.com>
2018-01-11 16:07:13 +0000

    Added terraform config and jenkins file for deployment on CNP environment (#3)
    * Added terraform config and jenkins file for deployment on CNP environment
    
    * Updated configuration and jenkins file as per review comments
    
    * Updated components to api as the product and component are concatenated
    
    * Updated jar version as per review comment
    
    * Removed echo from the jenkins file

---
+++ infrastructure/main.tf
@@ -0,0 +1,23 @@
+
+module "send-letter-service-api" {
+  source   = "git@github.com/hmcts/terraform-module-webapp.git"
+  product  = "${var.product}"
+  location = "${var.location_api}"
+  env      = "${var.env}"
+  ilbIp    = "${var.ilbIp}"
+
+  app_settings = {
+    REDIS_HOST                   = "${module.redis-cache.host_name}"
+    REDIS_PORT                   = "${module.redis-cache.redis_port}"
+    REDIS_PASSWORD               = "${module.redis-cache.access_key}"
+  }
+
+}
+
+module "redis-cache" {
+  source   = "git@github.com:contino/moj-module-redis?ref=master"
+  product  = "${var.product}-redis"
+  location = "${var.location_api}"
+  env      = "${var.env}"
+  subnetid = "${data.terraform_remote_state.core_apps_infrastructure.subnet_ids[2]}"
+}
---
+++ infrastructure/variables.tf
@@ -0,0 +1,17 @@
+variable "product" {
+  type    = "string"
+  default = "send-letter-service"
+}
+
+variable "location_api" {
+  type    = "string"
+  default = "UK South"
+}
+
+variable "env" {
+  type = "string"
+}
+
+variable "ilbIp" {}
+
+# endregion
