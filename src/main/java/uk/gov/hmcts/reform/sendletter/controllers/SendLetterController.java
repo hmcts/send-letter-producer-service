155e77811f03d023621652bf2eb062361605e744
Nitin Prabhu <nitinprabhu@users.noreply.github.com>
2018-01-15 12:23:26 +0000

    Added  Controller(initial) as it will unlock other stories (#6)
    * Added  Controller(initial) as it will unlock other stories
    
    * Fixed checkstyle violation
    
    * Updated path so that it complies with rest api standard

---
+++ src/main/java/uk/gov/hmcts/reform/sendletter/controllers/SendLetterController.java
@@ -0,0 +1,39 @@
+package uk.gov.hmcts.reform.sendletter.controllers;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import uk.gov.hmcts.reform.sendletter.model.Letter;
+import uk.gov.hmcts.reform.sendletter.services.LetterService;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping(
+    path = "letters",
+    produces = {MediaType.APPLICATION_JSON_VALUE}
+)
+public class SendLetterController {
+
+    private final LetterService letterService;
+
+    public SendLetterController(LetterService letterService) {
+        this.letterService = letterService;
+    }
+
+    @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE})
+    @ApiOperation(value = "Send letter to print and post service")
+    @ApiResponses({
+        @ApiResponse(code = 200, message = "Successfully sent letter"),
+    })
+    public void sendLetter(@RequestBody Map<String, String> addressDetails) {
+        // TODO: Currently all the inputs and outputs are not known.Need to update this later.
+        Letter letter = new Letter();
+        letterService.send(letter);
+    }
+}
