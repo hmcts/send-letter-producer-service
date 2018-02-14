15cede60318d038761082c3bf9b880a8a161a6ad
donatas M <doncem@users.noreply.github.com>
2018-02-14 16:51:58 +0000

    Add postgres container (#58)

--- bin/run-in-docker.sh
+++ bin/run-in-docker.sh
@@ -15,6 +15,7 @@ print_help() {
 
   Available parameters:
     APPLICATION_INSIGHTS_IKEY           Defaults to '00000000-0000-0000-0000-000000000000'
+    LETTER_TRACKING_DB_PASSWORD         Defaults to 'password'
     S2S_URL                             Defaults to 'false' - disables health check
     SERVICE_BUS_CONNECTION_STRING       Defaults to 'sb://namespace.servicebus.windows.net'
     SERVICE_QUEUE_MESSAGE_TTL_IN_DAYS   Default to '7'
@@ -27,6 +28,7 @@ GRADLE_INSTALL=false
 
 # environment variables
 APPLICATION_INSIGHTS_IKEY="00000000-0000-0000-0000-000000000000"
+LETTER_TRACKING_DB_PASSWORD="password"
 S2S_URL=false
 SERVICE_BUS_CONNECTION_STRING="sb://namespace.servicebus.windows.net"
 SERVICE_QUEUE_MESSAGE_TTL_IN_DAYS=7
@@ -49,6 +51,7 @@ execute_script() {
   echo "Assigning environment variables.."
 
   export APPLICATION_INSIGHTS_IKEY=${APPLICATION_INSIGHTS_IKEY}
+  export LETTER_TRACKING_DB_PASSWORD=${LETTER_TRACKING_DB_PASSWORD}
   export S2S_URL=${S2S_URL}
   export SERVICE_BUS_CONNECTION_STRING=${SERVICE_BUS_CONNECTION_STRING}
   export SERVICE_QUEUE_MESSAGE_TTL_IN_DAYS=${SERVICE_QUEUE_MESSAGE_TTL_IN_DAYS}
@@ -66,6 +69,7 @@ while true ; do
     -p|--param)
       case "$2" in
         APPLICATION_INSIGHTS_IKEY=*) APPLICATION_INSIGHTS_IKEY="${2#*=}" ; shift 2 ;;
+        LETTER_TRACKING_DB_PASSWORD=*) LETTER_TRACKING_DB_PASSWORD="${2#*=}" ; shift 2 ;;
         S2S_URL=*) S2S_URL="${2#*=}" ; shift 2 ;;
         SERVICE_BUS_CONNECTION_STRING=*) SERVICE_BUS_CONNECTION_STRING="${2#*=}" ; shift 2 ;;
         SERVICE_QUEUE_MESSAGE_TTL_IN_DAYS=*) SERVICE_QUEUE_MESSAGE_TTL_IN_DAYS="${2#*=}" ; shift 2 ;;
