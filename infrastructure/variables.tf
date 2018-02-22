03f4fdb378747d1e49f2eb75c565eded8ece8091
leszekg <gonczar@gmail.com>
2018-02-22 14:22:50 +0000

    Add missing input variables in Terraform code

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -32,6 +32,12 @@ variable "ilbIp" {}
 
 variable "tenant_id" {}
 
+variable "client_id" {
+  description = "(Required) The object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies. This is usually sourced from environment variables and not normally required to be specified."
+}
+
+variable "subscription" {}
+
 variable "jenkins_AAD_objectId" {
   type        = "string"
   description = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
