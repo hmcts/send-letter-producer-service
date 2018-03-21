5d382cc57e5aa1806400685350b62d91ca29bab0
leszekg <gonczar@gmail.com>
2018-03-21 16:56:41 +0000

    Fix content-type header in smoke tests

--- src/smokeTest/java/uk/gov/hmcts/reform/sendletter/controllers/SmokeTestSuite.java
+++ src/smokeTest/java/uk/gov/hmcts/reform/sendletter/controllers/SmokeTestSuite.java
@@ -17,7 +17,7 @@ import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
-import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RunWith(SpringRunner.class)
 @SuppressWarnings("PMD.AbstractClassWithoutAbstractMethod")
@@ -50,7 +50,7 @@ public abstract class SmokeTestSuite {
             .given()
             .relaxedHTTPSValidation()
             .baseUri(this.s2sUrl)
-            .header(CONTENT_TYPE, APPLICATION_JSON)
+            .header(CONTENT_TYPE, APPLICATION_JSON_VALUE)
             .body(params)
             .post("/lease")
             .andReturn();
