ad913a600404e5675170739e06a55835361186ed
Rafal Kondratowicz <rkondratowicz@users.noreply.github.com>
2018-03-21 15:34:40 +0000

    Check sign in status in smoke test (#143)

--- src/smokeTest/java/uk/gov/hmcts/reform/sendletter/controllers/SmokeTestSuite.java
+++ src/smokeTest/java/uk/gov/hmcts/reform/sendletter/controllers/SmokeTestSuite.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.reform.sendletter.controllers;
 
 import com.warrenstrange.googleauth.GoogleAuthenticator;
 import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -11,6 +12,8 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SuppressWarnings("PMD.AbstractClassWithoutAbstractMethod")
@@ -34,14 +37,18 @@ public abstract class SmokeTestSuite {
      * @return s2s JWT token.
      */
     protected String signIn() {
-        return RestAssured
+        Response response = RestAssured
             .given()
             .relaxedHTTPSValidation()
             .baseUri(this.s2sUrl)
             .formParam("microservice", this.s2sName)
             .formParam("oneTimePassword", new GoogleAuthenticator().getTotpPassword(this.s2sSecret))
             .post("/lease")
-            .andReturn()
+            .andReturn();
+
+        assertThat(response.getStatusCode()).isEqualTo(200);
+
+        return response
             .getBody()
             .print();
     }
